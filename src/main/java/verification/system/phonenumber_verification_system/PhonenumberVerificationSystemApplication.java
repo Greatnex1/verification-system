434f0b485acfe33ef63931c0231ea621d6ef3ee0
Nouah <noahgreatakoni@gmail.com>
2023-01-09 02:42:54 +0100

    main

---
+++ src/main/java/verification/system/phonenumber_verification_system/PhonenumberVerificationSystemApplication.java
@@ -0,0 +1,15 @@
+package verification.system.phonenumber_verification_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonenumberVerificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonenumberVerificationSystemApplication.class, args);
+	}
+
+//	System.getenv("TWILIO_ACCOUNT_SID")System.getenv("TWILIO_AUTH_TOKEN")
+
+}
